508572299afb75c4cf49145059154185a015645b
Alex Leandro <alexleandro846@gmail.com>
2026-04-14 12:53:39 -0600

    Modulo de Registro en Api

---
+++ API_Practica3/API_Practica3/Controllers/AbonoController.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using API_Practica3.Models;
+
+namespace API_Practica3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AbonoController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public AbonoController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET api/Abono/ComprasPendientes
+        [HttpGet("ComprasPendientes")]
+        public IActionResult ComprasPendientes()
+        {
+            using var cn = new SqlConnection(
+                _config.GetValue<string>("ConnectionStrings:DefaultConnection"));
+
+            var lista = cn.Query<CompraConsulta>(
+                "SP_ConsultarComprasPendientes",
+                commandType: CommandType.StoredProcedure
+            ).ToList();
+
+            return Ok(lista);
+        }
+
+        // GET api/Abono/SaldoCompra/{id}
+        [HttpGet("SaldoCompra/{id}")]
+        public IActionResult SaldoCompra(long id)
+        {
+            using var cn = new SqlConnection(
+                _config.GetValue<string>("ConnectionStrings:DefaultConnection"));
+
+            var saldo = cn.QueryFirstOrDefault<decimal>(
+                "SELECT Saldo FROM Principal WHERE Id_Compra = @Id",
+                new { Id = id }
+            );
+
+            return Ok(new { saldo });
+        }
+
+        // POST api/Abono/Registrar
+        [HttpPost("Registrar")]
+        public IActionResult Registrar([FromBody] AbonoRequest request)
+        {
+            using var cn = new SqlConnection(
+                _config.GetValue<string>("ConnectionStrings:DefaultConnection"));
+
+            var parametros = new DynamicParameters();
+            parametros.Add("@Id_Compra", request.Id_Compra);
+            parametros.Add("@Monto", request.Monto);
+
+            cn.Execute(
+                "SP_RegistrarAbono",
+                parametros,
+                commandType: CommandType.StoredProcedure
+            );
+
+            return Ok(new { mensaje = "Abono registrado correctamente." });
+        }
+    }
+}
---
+++ API_Practica3/API_Practica3/Models/AbonoRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Practica3.Models
+{
+    public class AbonoRequest
+    {
+        public long Id_Compra { get; set; }
+        public decimal Monto { get; set; }
+    }
+}
